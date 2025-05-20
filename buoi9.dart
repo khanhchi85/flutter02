c21a347e9fb7194a75f86506d5e6eb59a74998a8
khanhchi85 <khanhchileke@gmail.com>
2025-05-20 23:20:09 +0900

    Update buoi9.dart

--- buoi9.dart
+++ buoi9.dart
@@ -1,3 +1,4 @@
+/*
 class Student {
   int id; 
   String name; 
@@ -71,4 +72,63 @@ void main() {
   manager.findStudentById(112233);
   print('\n');
   manager.findStudentById(999999); 
+}
+*/
+class BankAccount {
+  String accountNumber; 
+  String ownerName; 
+  double balance; 
+
+  
+  BankAccount(this.accountNumber, this.ownerName, this.balance);
+
+  
+  void deposit(double amount) {
+    if (amount > 0) {
+      balance += amount;
+      print('Đã gửi $amount vào tài khoản. Số dư mới: $balance');
+    } else {
+      print('Số tiền gửi phải lớn hơn 0.');
+    }
+  }
+
+  
+  void withdraw(double amount) {
+    if (amount > 0 && amount <= balance) {
+      balance -= amount;
+      print('Đã rút $amount từ tài khoản. Số dư mới: $balance');
+    } else if (amount <= 0) {
+      print('Số tiền rút phải lớn hơn 0.');
+    } else {
+      print('Số dư không đủ để rút $amount.');
+    }
+  }
+
+  
+  double checkBalance() {
+    return balance;
+  }
+
+  
+  void displayBalance() {
+    print('Tài khoản $accountNumber (Chủ tài khoản: $ownerName) - Số dư: $balance');
+  }
+}
+
+void main() {
+  
+  BankAccount account = BankAccount('123456789', 'Khanh Chi', 1000.0);
+
+  
+  print('--- Kiểm tra hoạt động tài khoản ---');
+  account.displayBalance(); 
+
+  account.deposit(500.0); 
+  account.withdraw(200.0); 
+  account.withdraw(2000.0); 
+  account.deposit(-100.0); 
+  account.withdraw(0.0); 
+
+  print('\nSố dư cuối cùng: ${account.checkBalance()}');
+  account.displayBalance(); 
 }
